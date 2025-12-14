bb10ca06c4f1b80f59121492d611d0582931d472
shinchaerin79 <shinchaerin79@gmail.com>
2025-12-14 17:53:15 +0900

    :sparkles: Feat: Update A and B

--- GitPractice/src/mygit/B.java
+++ GitPractice/src/mygit/B.java
@@ -3,5 +3,6 @@ package mygit;
 public class B {
     public void print() {
         System.out.println("B v1: base version");
+        System.out.println("B status: updated in Bob branch");
     }
 }
